dfe9760dea480df4b8152fcfdf12baff13063082
MateuszCiupa <01131212@pw.edu.pl>
2020-05-29 15:04:01 +0200

    gql index

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import "reflect-metadata";
+import "dotenv/config";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { UserResolver } from "./resolvers";
+import { buildSchema } from "type-graphql";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import {
+  getAccessToken,
+  getRefreshToken,
+  sendRefreshToken,
+} from "./util/auth";
+import { User } from "./entity/User";
+
+(async () => {
+  const app = express();
+  app.use(cookieParser());
+
+  app.get("/", (_req, res) => res.send("Graphql on: /graphql"));
+
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    sendRefreshToken(res, getRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: getAccessToken(user) });
+  });
+
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => console.log("Server is listening on port 4000"));
+})();
